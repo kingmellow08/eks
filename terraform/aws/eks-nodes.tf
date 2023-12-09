dd21467ba38eaa8587b634ea8935c2797bca8bfa
kingmellow08 <103248071+kingmellow08@users.noreply.github.com>
2023-12-09 16:29:54 -0500

    Update eks-nodes.tf

--- terraform/aws/eks-nodes.tf
+++ terraform/aws/eks-nodes.tf
@@ -1,1 +1,109 @@
 
+resource "aws_iam_role" "nodes" {
+  name = "eks-nodes"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_eks_node_group" "priv-nodes-eu-north-1a" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "priv-nodes-eu-north-1a"
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids = [
+    aws_subnet.priv-eu-north-1a.id
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_eks_node_group" "priv-nodes-eu-north-1b" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "priv-nodes-eu-north-1b"
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids = [
+    aws_subnet.priv-eu-north-1b.id
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_eks_node_group" "priv-nodes-eu-north-1c" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "priv-nodes-eu-north-1c"
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids = [
+    aws_subnet.priv-eu-north-1c.id
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
