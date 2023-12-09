529576ebbf8c903e029c5360ac85447a742ba2f5
kingmellow08 <103248071+kingmellow08@users.noreply.github.com>
2023-12-09 16:29:14 -0500

    Update eks-cluster.tf
    added code to this file

--- terraform/aws/eks-cluster.tf
+++ terraform/aws/eks-cluster.tf
@@ -1,1 +1,67 @@
+resource "aws_iam_role" "eks-cluster" {
+  name = "eks-cluster"
 
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_security_group" "eks-cluster-sg" {
+  name        = "eks-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-sg"
+  }
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+    security_group_ids = [aws_security_group.eks-cluster-sg.id]
+    subnet_ids = [
+      aws_subnet.priv-eu-north-1a.id,
+      aws_subnet.priv-eu-north-1b.id,
+      aws_subnet.priv-eu-north-1c.id,
+      aws_subnet.pub-eu-north-1a.id,
+      aws_subnet.pub-eu-north-1b.id,
+      aws_subnet.pub-eu-north-1c.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy,
+  ]
+}
